9aa0ea44be60d5feaf136c59691b59b18efc3474
KamiKozy <kamikozytpp@gmail.com>
2018-07-05 17:49:38 -0400

    started some CSS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,5 @@
 $(function () {
 
-
     var myQuiz = {
         questions: [
             "1) What town does the Simpson family live in?",
@@ -16,7 +15,7 @@ $(function () {
             ["Animal Shelter", "Puppy Mill", "Adoption Fair", "Dog Tracks"],
             ["Wiggum", "Lou", "Garcia", "fitz"],
         ],
-        correctAnswers: { // questionNumber : answer index
+        correctAnswers: { // questionNumber : correct answer index
             "0": "1",
             "1": "2",
             "2": "0",
@@ -25,49 +24,52 @@ $(function () {
         }
     }
 
+
+
     function makeQuestions() {
 
-        for (i = 0; i < myQuiz.questions.length; i++) { //checks number of questions, allows scaling
+        for (i = 0; i < myQuiz.questions.length; i++) {
             var value = 0
             var question = myQuiz.questions[i];
-            var $questionBox = $("<div>", { class: "questionBox" }).append("<h3>"+question);
-            var $answerBox = $("<div>", { class: "answerBox" }) //creates the div to add options
+            var $questionBox = $("<div>", { class: "questionBox" }).append("<h3>" + question);
+            var $answerBox = $("<div>", { class: "answerBox" })
 
             $("#quiz").append($questionBox).append($answerBox)
 
-            myQuiz.choices[i].forEach(function (option) {  //Combination of forEach and increasing counter lets me set increasing value. allows me to change number of choices
+            myQuiz.choices[i].forEach(function (option) {
                 $($answerBox).append("<label><input id='radioButton' type='radio' name=" + i + " value=" + value + ">" + option)
                 value++;
             })
-
-
-
-
-
         }
     }
-    makeQuestions()
+    $("#start").on('click', function () {
+        $("#content").css('display', 'block');
+        $(this).remove();
+    })
+    $("#submit").on('click', function createScore() {
+        var correct = 0
 
-
-
-
-
-
-    $("#submit").on('click', function () {
-
-        $answerBox = $(".answerBox"); // checks for each answerBox which is checked
+        $(this).remove()  
+        $answerBox = $(".answerBox");
         $answerBox.each(function () {
             var answer = $(this).find("input:checked");
-            key = answer.attr("name")   // pulls out name/value to check against answers array
-            val = answer.attr("value")
-
+            var key = answer.attr("name")
+            var val = answer.attr("value")
             if (myQuiz.correctAnswers[key] !== val) {
                 var correctAnswer = myQuiz.correctAnswers[key]
-                $("#incorrect").append(myQuiz.questions[key] + "<span class='wrongAnswer'> " + myQuiz.choices[key][val] +"</span> <br> <div class='correctAnswer'>Correct Answer: "+ myQuiz.choices[key][correctAnswer]+ "</div")
+                $("#incorrect").append(myQuiz.questions[key] + "<span class='wrongAnswer'> " + myQuiz.choices[key][val] + "</span> <br> <div class='correctAnswer'>Correct Answer: " + myQuiz.choices[key][correctAnswer] + "</div><br>")
             }
             else if (myQuiz.correctAnswers[key] === val) {
+                correct++;
             }
         })
+        $("#quiz").remove();
+        var totalCorrect = correct * 20;
+        $("#tally").css('display','block')
+        $("#score").append("<h2> Final Score: " + totalCorrect + "%")
+        
 
     })
+
+    makeQuestions()
 })
